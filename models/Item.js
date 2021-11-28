770efff6f77ea34d7740ae4115606b4b14ecb946
lizetterdz <71050320+lizetterdz@users.noreply.github.com>
2021-11-28 15:24:54 -0600

    added sale count control

--- models/Item.js
+++ models/Item.js
@@ -3,7 +3,8 @@ const { model, Schema } = require('mongoose');
 const itemSchema = new Schema({
     name: { type: String, required: true },
     price: { type: Number, required: true },
-    stock: { type: Number, required: true},
+    stock: { type: Number, required: true },
+    timesSold: { type: Number, required: true },
 });
 
 const Item = model('Items', itemSchema);
