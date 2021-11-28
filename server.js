ccf7591977b12404caf25ca015c177ad219f4cd7
lizetterdz <71050320+lizetterdz@users.noreply.github.com>
2021-11-28 01:42:01 -0600

    port change

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = 5001;  
+const PORT = process.env.PORT || 5000;  
 
 // Router
 const routes = require('./routes/router');
