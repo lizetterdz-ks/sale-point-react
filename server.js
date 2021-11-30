80be4003afdee485fb4bd3d3a271be28db72a038
lizetterdz <71050320+lizetterdz@users.noreply.github.com>
2021-11-29 18:46:19 -0600

    cors added

--- server.js
+++ server.js
@@ -12,7 +12,13 @@ const initializeDataBases = require('./infrastructure/');
 // Middleware to parse body
 app.use(express.json());
 
-app.use(cors());
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
 
 // Define routes
 app.use(routes);
